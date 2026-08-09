fda388bbade2126a197e5e0409399ae5971d7d9e
knownaskira <irmansyahmuslimin05@gmail.com>
2026-08-09 21:04:06 +0800

    Update Migration

--- database/migrations/2025_05_26_193020_create_pelanggaran_table.php
+++ database/migrations/2025_05_26_193020_create_pelanggaran_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('pelanggarans', function (Blueprint $table) {
             $table->id();
-            $table->enum('kategori', ['ringan', 'sedang', 'berat']);
+            $table->enum('kategori', ['ringan', 'sedang', 'sangat berat']);
             $table->text('deskripsi');
             $table->unsignedInteger('skor');
             $table->timestamps();
